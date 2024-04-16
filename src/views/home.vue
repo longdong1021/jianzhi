acc42355aefc6d00af3cd1dd89912355ad73b418
wangld0 <longdong1021@163.com>
2024-04-16 19:13:28 +0800

    1.0.1 分支修改提交

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>test</div>
+  <div>
+    <ul>
+        <li>test 001</li>
+        <li>test 002</li>
+        <li>test 003</li>
+        <li>test 004</li>
+    </ul>
+  </div>
 </template>
 
 <script>
